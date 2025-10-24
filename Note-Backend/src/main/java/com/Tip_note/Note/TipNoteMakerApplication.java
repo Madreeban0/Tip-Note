83029347402e5afe0d0b8fe9136ca8bce36dc5bc
Madreeban0 <madreeban277@gmail.com>
2025-10-24 10:33:14 +0530

    first commit

---
+++ Note-Backend/src/main/java/com/Tip_note/Note/TipNoteMakerApplication.java
@@ -0,0 +1,13 @@
+package com.Tip_note.Note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipNoteMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipNoteMakerApplication.class, args);
+	}
+
+}
---
+++ Note-Backend/src/test/java/com/Tip_note/Note/TipNoteMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tip_note.Note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipNoteMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
