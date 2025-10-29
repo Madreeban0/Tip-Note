792a57ebbddb9c7502fa193ffbdc7a768e4c77b2
Madreeban0 <madreeban277@gmail.com>
2025-10-29 14:34:53 +0530

    updated backend

--- Note-Backend/src/main/java/com/Tip_note/Note/Controllers/noteController.java
+++ Note-Backend/src/main/java/com/Tip_note/Note/Controllers/noteController.java
@@ -1,16 +1,22 @@
 package com.Tip_note.Note.Controllers;
 
-import com.Tip_note.Note.Models.Note;
-import com.Tip_note.Note.Repository.noteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Tip_note.Note.Models.Note;
+import com.Tip_note.Note.Repository.noteRepository;
 
 
 
@@ -18,7 +24,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://tippnote.netlify.app/")
+@CrossOrigin(origins = "https://tippnote.netlify.app")
 public class noteController {
     
     @Autowired
