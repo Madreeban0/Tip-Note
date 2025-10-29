154ec0452b060e404b14a2a638ad4d79c8b8cb27
Madreeban0 <madreeban277@gmail.com>
2025-10-29 16:12:30 +0530

    updated backend

---
+++ Note-Backend/src/main/java/com/Tip_note/Note/Config/Webconfig.java
@@ -0,0 +1,18 @@
+package com.Tip_note.Note.Config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class Webconfig implements WebMvcConfigurer {
+     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow CORS for all endpoints
+            .allowedOrigins("https://tippnote.netlify.app") // Your Netlify frontend URL
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
+            .allowedHeaders("*") // Allow all headers
+            .allowCredentials(true); // Allow credentials
+    }
+}
