97693b949306ab5b481f3326829d7094e71ec3be
Madreeban0 <madreeban277@gmail.com>
2025-10-30 16:21:08 +0530

    Generated Jwt secret key

--- Note-Backend/src/main/java/com/Tip_note/Note/Security/GenerateKey.java
+++ Note-Backend/src/main/java/com/Tip_note/Note/Security/GenerateKey.java
@@ -8,16 +8,15 @@ public class GenerateKey {
         
         SecureRandom secureRandom = new SecureRandom();
         
+    
         byte[] keyBytes = new byte[32];
         
         secureRandom.nextBytes(keyBytes);
         
-        
+    
         String base64Key = Base64.getEncoder().encodeToString(keyBytes);
-        
-        
+   
         System.out.println("Your new JWT Secret Key (Base64):");
         System.out.println(base64Key);
     }
 }
-
