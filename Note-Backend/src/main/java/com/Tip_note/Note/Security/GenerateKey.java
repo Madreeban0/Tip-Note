5487fdd39adc30dcd9d715644a4716d1793002e0
Madreeban0 <madreeban277@gmail.com>
2025-10-30 16:11:32 +0530

    Updated JWT key update

---
+++ Note-Backend/src/main/java/com/Tip_note/Note/Security/GenerateKey.java
@@ -0,0 +1,23 @@
+package com.Tip_note.Note.Security;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class GenerateKey {
+    public static void main(String[] args) {
+        
+        SecureRandom secureRandom = new SecureRandom();
+        
+        byte[] keyBytes = new byte[32];
+        
+        secureRandom.nextBytes(keyBytes);
+        
+        
+        String base64Key = Base64.getEncoder().encodeToString(keyBytes);
+        
+        
+        System.out.println("Your new JWT Secret Key (Base64):");
+        System.out.println(base64Key);
+    }
+}
+
